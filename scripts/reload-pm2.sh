444a175a64136412e8497f64f06f87b227dcc259
suppKimKr <byjiwon@korea.ac.kr>
2022-12-22 16:20:34 +0900

    init

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -3,7 +3,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-REPOSITORY=/var/www/html/21market-be-v3
+REPOSITORY=/var/www/html/nest-base
 
 echo $HOME
 echo $DEPLOYMENT_GROUP_NAME
